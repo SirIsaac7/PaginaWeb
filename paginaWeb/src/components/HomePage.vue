e08db0d118e1cdefd341e1daa38b4dd91db6affd
SirIsaac7 <isaacat7@gmail.com>
2025-11-11 19:56:45 -0400

    Cambios por Isaac Alejandro Tola

---
+++ paginaWeb/src/components/HomePage.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="home-hero">
+    <div class="home-bg"></div>
+    <div class="home-content">
+      <h1 class="home-title">AJAYU LA SOMBRA DE LA PAZ</h1>
+      <p class="home-desc">Explora, supera desafíos y conviértete en el héroe. ¡Comienza tu viaje ahora!</p>
+      <button class="home-btn">Jugar Ahora</button>
+      <div class="home-features">
+        <h2>Características</h2>
+        <ul>
+          <li>🎮 Jugabilidad inmersiva</li>
+          <li>🌎 Mundo abierto</li>
+          <li>⚔️ Batallas épicas</li>
+          <li>🧩 Puzzles y retos</li>
+        </ul>
+      </div>
+      <div class="home-social">
+        <h2>Síguenos</h2>
+        <a href="#" class="social-icon twitter" title="Twitter">
+            <box-icon name='twitter' type='logo' ></box-icon>
+        </a>
+        <a href="#" class="social-icon facebook" title="Facebook">
+            <box-icon type='logo' name='facebook-square'></box-icon>
+        </a>
+        <a href="#" class="social-icon instagram" title="Instagram">
+            <box-icon name='instagram-alt' type='logo' ></box-icon>
+        </a>
+        <a href="#" class="social-icon youtube" title="YouTube">
+            <box-icon name='youtube' type='logo' ></box-icon>
+        </a>
+      </div>
+    </div>
+  </section>
+</template>
+<script setup lang="ts">
+import '../assets/home.css'
+</script>
